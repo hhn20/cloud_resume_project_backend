13fb5f9fe6a049b20e0cdb06bcf919413426e3ea
hhnvel <harsha@velmeni.com>
2024-06-19 20:20:35 -0700

    first push

---
+++ app.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+
+
+
+def lambda_handler(event, context):
+    # Increment the count
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('VISIT_COUNT')
+        response = table.update_item(
+            Key={'id': 1},
+            UpdateExpression='ADD visitors :incr',
+            ExpressionAttributeValues={':incr': 1},
+            ReturnValues='UPDATED_NEW'
+        )
+        
+        # Get the updated count
+        new_count = response['Attributes']
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': new_count
+        }
+    except Exception as e:
+         return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': str(e)
+        }
